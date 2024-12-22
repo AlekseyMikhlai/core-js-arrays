483d8653aadba81ff52df978fa4c5683a14e55e5
Aleksey Mikhlai <amikhlai4@gmail.com>
2024-12-22 17:09:30 +0300

    feat: solution for first and second exercises

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -20,8 +20,9 @@
  *    getIntervalArray(0, 100) => [ 0, 1, 2, ..., 100 ]
  *    getIntervalArray(3, 3) => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  const arr = Array.from({ length: end - start + 1 }, (v, i) => start + i);
+  return arr;
 }
 
 /**
@@ -37,8 +38,18 @@ function getIntervalArray(/* start, end */) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  let sum = 0;
+  if (arr1.length > arr2.length) {
+    sum = arr1.map((item, i) =>
+      arr2[i] === undefined ? item : item + arr2[i]
+    );
+  } else {
+    sum = arr2.map((item, i) =>
+      arr1[i] === undefined ? item : item + arr1[i]
+    );
+  }
+  return sum;
 }
 
 /**
@@ -53,8 +64,8 @@ function sumArrays(/* arr1, arr2 */) {
  *    findElement(['Array', 'Number', 'string'], 'Date') => -1
  *    findElement([0, 1, 2, 3, 4, 5], 5) => 5
  */
-function findElement(/* arr, value */) {
-  throw new Error('Not implemented');
+function findElement(arr, value) {
+  
 }
 
 /**
